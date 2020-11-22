921ef8a435a29426f62e61d12db7352f2a346d07
austinvhuang <austinh@alum.mit.edu>
2020-11-22 12:40:14 -0500

    build tutorial notes

---
+++ wasm-tutorial/emscripten/demo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("WASM Ready - with HTML\n");
+  return 1;
+}
--- wasm-tutorial/raw/server.js
+++ wasm-tutorial/raw/server.js
@@ -1,7 +1,5 @@
 import express from 'express';
 const app = express();
 
-app.use(express.static('public'))
-
 app.use(express.static('public'));
 app.listen(2222, () => console.log('Server running on port 2222'));
