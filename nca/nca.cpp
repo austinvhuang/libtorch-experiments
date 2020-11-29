f32ebd52e2799e3a89ea9bc4f180b8cb6e8f18ad
austinvhuang <austinh@alum.mit.edu>
2020-11-28 20:17:03 -0500

    match dimensions end-to-end

--- nca/nca.cpp
+++ nca/nca.cpp
@@ -35,9 +35,10 @@ std::pair<Tensor, Tensor> create_sobel(int n_channels) {
   const int duplicate_dim = 1;
   float sx[1][1][3][3] = {{{{-1., 0., 1.}, {-2., 0., 2.}, {-1., 0., 1.}}}};
   const auto sobel_x1 = torch::from_blob(sx, {1, 1, 3, 3});
-  const auto sobel_y1 = torch::transpose(sobel_x1, 0, -1);
-  return {repeat_n(sobel_x1, n_channels, duplicate_dim),
-          repeat_n(sobel_y1, n_channels, duplicate_dim)};
+  const auto sobel_y1 = torch::transpose(sobel_x1, 3, -1);
+  return {sobel_x1, sobel_y1};
+  //  return {repeat_n(sobel_x1, n_channels, duplicate_dim),
+  //          repeat_n(sobel_y1, n_channels, duplicate_dim)};
 }
 
 /* Model */
@@ -52,13 +53,26 @@ Tensor perceive(const Tensor state_grid) {
   //
   // We concatenate those gradients with the cells own states, forming a
   // percepted vector, for each cell.
+
+  assert(state_grid.sizes()[0] == 1);
+
   auto [sobel_x, sobel_y] = create_sobel(4);
-  const auto grad_x = torch::conv2d(state_grid, sobel_x);
-  const auto grad_y = torch::conv2d(state_grid, sobel_y);
+
+  auto state_grid_flat = torch::transpose(state_grid, 0, 1);
+
+  const auto grad_x =
+      torch::transpose(torch::conv2d(state_grid_flat, sobel_x, {}, 1, 1), 0, 1);
+  const auto grad_y =
+      torch::transpose(torch::conv2d(state_grid_flat, sobel_y, {}, 1, 1), 0, 1);
+
+  std::cout << " sobel_x " << sobel_x.sizes() << std::endl;
+  std::cout << " sobel_y " << sobel_y.sizes() << std::endl;
+  std::cout << " grad_x " << grad_x.sizes() << std::endl;
+  std::cout << " grad_y " << grad_y.sizes() << std::endl;
+  std::cout << " state_ " << state_grid.sizes() << std::endl;
   std::vector<Tensor> perception_vec = {state_grid, grad_x, grad_y};
-  // auto perception_grid = torch::cat(perception_vec, 2);
-  // TODO fix padding dimensions
-  auto perception_grid = state_grid;
+  auto perception_grid = torch::cat(perception_vec, 1);
+  std::cout << " perception_grid " << perception_grid.sizes() << std::endl;
   return perception_grid;
 }
 
@@ -74,6 +88,10 @@ Tensor update(const Tensor perception_vector, const Tensor fc1,
   // The update rule is designed to exhibit “do-nothing” initial behaviour -
   // implemented by initializing the weights of the final convolutional layer in
   // the update rule with zero.
+  std::cout << "perception_vector " << perception_vector.sizes() << std::endl;
+  std::cout << "fc1 " << fc1.sizes() << std::endl;
+  std::cout << "fc2 " << fc2.sizes() << std::endl;
+
   auto x = torch::linear(perception_vector, fc1);
   x = torch::relu(x);
   auto ds = torch::linear(x, fc2);
@@ -85,8 +103,11 @@ Tensor stochastic_update(const Tensor state_grid, const Tensor ds_grid) {
   // values to 0with some predefined probability (we use 0.5 during training).
   // This operation can be also seen as an application of per-cell dropout to
   // update vectors.
-  auto rand_mask = torch::_cast_Float(rand_like(state_grid).gt(0.5));
-  return state_grid + ds_grid;
+  //
+  assert(state_grid.sizes() == ds_grid.sizes());
+  auto rand_mask = torch::_cast_Float(rand_like(ds_grid).gt(0.5));
+  auto ds_masked = ds_grid * rand_mask;
+  return state_grid + ds_masked;
 }
 
 Tensor alive_masking(const Tensor state_grid) {
@@ -96,37 +117,57 @@ Tensor alive_masking(const Tensor state_grid) {
   // We enforce this by explicitly setting all channels of empty cells to zeros.
   // A cell is considered empty if there is no “mature” (alpha>0.1) cell in its
   // 3x3 neightborhood.
+  auto alpha =
+      state_grid.index({Slice(), 3, Slice(), Slice()}); // 3 = alpha channel
+  std::cout << "alpha " << alpha.sizes() << std::endl;
   auto alive =
-      torch::max_pool2d(state_grid.index({Slice(), Slice(), 3}), {3, 3})
-          .gt(0.1);
+      torch::max_pool2d(alpha, {3, 3}, {1}, {1}).gt(0.1); // stride=1, padding=1
+  std::cout << "alive pre " << alive.sizes() << std::endl;
+  alive = torch::reshape(alive, {1, 1, alpha.sizes()[1], alpha.sizes()[2]});
+  std::cout << "alive post " << alive.sizes() << std::endl;
+  alive = repeat_n(alive, state_grid.sizes()[1], 1);
+  std::cout << "alive rep " << alive.sizes() << std::endl;
   return state_grid * torch::_cast_Float(alive);
+  return state_grid;
 }
 
 struct NCA : torch::nn::Module {
   NCA() {}
   Tensor forward(Tensor state_grid) {
-    auto perception_vector = perceive(state_grid);
-    auto ds = update(perception_vector, fc1, fc2);
+    auto perception_grid = perceive(state_grid);
+
+    // move channels to the last dimension for the dense layer computation
+    auto perception_vec =
+        torch::transpose(torch::transpose(perception_grid, 1, 2), 2, 3);
+    std::cout << "perception_vec " << perception_vec.sizes() << std::endl;
+
+    auto ds = update(perception_vec, fc1, fc2);
+    ds = torch::transpose(torch::transpose(ds, 2, 3), 1, 2);
+    std::cout << "state_grid " << state_grid.sizes() << std::endl;
+    std::cout << "ds         " << ds.sizes() << std::endl;
     auto updated = stochastic_update(state_grid, ds);
+    std::cout << "updated    " << updated.sizes() << std::endl;
     auto masked = alive_masking(updated);
     return masked;
   }
-  Tensor fc1 = torch::zeros({16, 16});
-  Tensor fc2 = torch::zeros({16, 16});
+  Tensor fc1;
+  Tensor fc2;
 };
 
 int main(int argc, char *argv[]) {
   auto world = init_world(WorldDim{4, 8, 8});
   world.index_put_({0, 0, Slice(), 5}, 1.0);
   world.index_put_({0, 0, 5, Slice()}, 1.0);
+  std::cout << "World" << std::endl << world << std::endl;
 
-  std::cout << world << std::endl;
-
+  const int fc1_input_dim = (4 + 2 * 4);
   auto nca = NCA();
+  nca.fc1 = torch::zeros({128, fc1_input_dim});
+  nca.fc2 = torch::zeros({4, 128});
+  auto fwd = nca.forward(world);
 
-  auto out = perceive(world);
-
-  std::cout << out << std::endl;
+  // auto out = perceive(world);
+  // std::cout << out << std::endl;
 
   std::cout << "Done" << std::endl;
 }
