26f409f6934e0c93ab416d57252adf87de18ada9
austinvhuang <austinh@alum.mit.edu>
2020-12-19 00:33:58 -0500

    add output, write ppm images, cleanup

--- nca/nca.cpp
+++ nca/nca.cpp
@@ -5,7 +5,7 @@
 using namespace torch::indexing;
 using torch::Tensor;
 
-// #define USE_GPU
+#define USE_GPU
 #ifdef USE_GPU
 const auto options = torch::TensorOptions().device(torch::kCUDA, 0);
 #else
@@ -22,11 +22,27 @@ void log(std::string text, Tensor value) {
   std::cout << text << std::endl << value << std::endl;
 }
 
-Tensor alpha2state(const Tensor alpha) {
-  // cells having \alpha > 0.1α>0.1 and their neighbors are considered
-  // “living”. Other cells are “dead” or empty and have their state vector
-  // values explicitly set to 0.0 at each time step.
-  return torch::sign(alpha - 0.1) / 2.0 + 0.5;
+void write_ppm(const Tensor img, std::ostream &o) {
+
+  assert(img.sizes()[0] == 4);
+  const int img_width = img.sizes()[2];
+  const int img_height = img.sizes()[1];
+
+  o << "P3\n" << img_width << ' ' << img_height << "\n255\n";
+
+  for (int i = 0; i < img_height; ++i) {
+    for (int j = 0; j < img_width; ++j) {
+      auto alpha = 1.0; // img[3][i][j];
+      float r = (img[0][i][j] * alpha).item<float>();
+      float g = (img[1][i][j] * alpha).item<float>();
+      float b = (img[2][i][j] * alpha).item<float>();
+      int ir = static_cast<int>(255.999 * r);
+      int ig = static_cast<int>(255.999 * g);
+      int ib = static_cast<int>(255.999 * b);
+
+      o << ir << ' ' << ig << ' ' << ib << '\n';
+    }
+  }
 }
 
 Tensor repeat_n(const Tensor t, const int n, const int dim) {
@@ -39,22 +55,6 @@ Tensor repeat_n(const Tensor t, const int n, const int dim) {
 Tensor init_world(const WorldDim dim) {
   return torch::zeros({1, dim.channels, dim.height, dim.width}, options);
 }
-
-/*
-// do not use - values get corrupted
-std::pair<Tensor, Tensor> create_sobel(int n_channels) {
-  const int duplicate_dim = 1;
-  float sx[1][1][3][3] = {{{{-1., 0., 1.}, {-2., 0., 2.}, {-1., 0., 1.}}}};
-  // const auto sobel_x1 = torch::from_blob(sx, {1, 1, 3, 3}, options);
-  // const auto sobel_x0 = torch::from_blob(sx, {1, 1, 3, 3});
-  // auto sobel_x1 = sobel_x0.to(torch::kCUDA);
-  auto sobel_x1 = sobel_x0.to(options.device_.type_);
-  const auto sobel_y1 = torch::transpose(sobel_x1, 3, -1);
-  log("sobel_x1", sobel_x1);
-  log("sobel_y1", sobel_y1);
-  return std::make_pair(sobel_x1, sobel_y1);
-}
-*/
 
 /* Model */
 
@@ -72,18 +72,14 @@ Tensor perceive(const Tensor &state_grid) {
   assert(state_grid.sizes()[0] == 1);
 
   // auto [sobel_x, sobel_y] = create_sobel(4);
-  const int duplicate_dim = 1;
   float sx[1][1][3][3] = {{{{-1., 0., 1.}, {-2., 0., 2.}, {-1., 0., 1.}}}};
-  const auto sobel_x0 = torch::from_blob(sx, {1, 1, 3, 3});
-  auto sobel_x = sobel_x0.to(options);
+  const auto sobel_x0 = torch::from_blob(sx, {1, 1, 3, 3}) /
+                        8.0; // divide by 8.0 to conserve intensity
+  auto sobel_x =
+      sobel_x0.to(options); // cannot initialize on the gpu using from_blob
   const auto sobel_y = torch::transpose(sobel_x, 3, -1);
 
   auto state_grid_flat = torch::transpose(state_grid, 0, 1);
-
-  //  std::cout << "sobel_x" << std::endl << sobel_x << std::endl;
-  //  std::cout << "sobel_y" << std::endl << sobel_x << std::endl;
-  //  std::cout << "state_grid_flat" << std::endl << state_grid_flat <<
-  //  std::endl;
 
   auto grad_x =
       torch::transpose(torch::conv2d(state_grid_flat, sobel_x, {}, 1, 1), 0, 1);
@@ -119,34 +115,35 @@ Tensor update(const Tensor &perception_vector, const Tensor &fc1,
   return ds;
 }
 
-Tensor stochastic_update(const Tensor state_grid, const Tensor ds_grid) {
+Tensor stochastic_update(const Tensor &state_grid, const Tensor &ds_grid) {
   // we apply a random per-cell mask to update vectors, setting all update
   // values to 0with some predefined probability (we use 0.5 during training).
   // This operation can be also seen as an application of per-cell dropout to
   // update vectors.
   //
-  auto update_threshold = 0.5;
+  const float update_threshold = 0.5;
   assert(state_grid.sizes() == ds_grid.sizes());
   auto rand_mask = torch::_cast_Float(rand_like(ds_grid).gt(update_threshold));
   auto ds_masked = ds_grid * rand_mask;
   return state_grid + ds_masked;
 }
 
-Tensor alive_masking(const Tensor state_grid) {
+Tensor alive_masking(const Tensor &state_grid) {
   // we don’t want empty cells to participate in computations or carry any
   // hidden state.
   //
   // We enforce this by explicitly setting all channels of empty cells to
   // zeros. A cell is considered empty if there is no “mature” (alpha>0.1)
   // cell in its 3x3 neightborhood.
+  const float threshold = 0.1;
   auto alpha =
       state_grid.index({Slice(), 3, Slice(), Slice()}); // 3 = alpha channel
-  auto alive =
-      torch::max_pool2d(alpha, {3, 3}, {1}, {1}).gt(0.1); // stride=1, padding=1
+  auto alive = torch::max_pool2d(alpha, {3, 3}, {1}, {1})
+                   .gt(threshold); // stride=1, padding=1
   alive = torch::reshape(alive, {1, 1, alpha.sizes()[1], alpha.sizes()[2]});
-  alive = repeat_n(alive, state_grid.sizes()[1], 1);
+  alive = repeat_n(alive, state_grid.sizes()[1],
+                   1); // repeat alive mask all channels
   return state_grid * torch::_cast_Float(alive);
-  return state_grid;
 }
 
 struct NCA : torch::nn::Module {
@@ -162,31 +159,35 @@ struct NCA : torch::nn::Module {
     auto ds = update(perception_vec, fc1, fc2);
     ds = torch::transpose(torch::transpose(ds, 2, 3), 1, 2);
     auto updated = stochastic_update(state_grid, ds);
-    // auto masked = alive_masking(updated);
-    // return masked;
-    return updated;
+    auto masked = alive_masking(updated);
+    return masked;
   }
   Tensor fc1;
   Tensor fc2;
 };
 
-void train(NCA nca, const Tensor &target, const Tensor &init) {
-  // TODO
-  const int n_iter = 3000;
-  const int t_max = 10;
+void train(NCA nca, const Tensor &target, const Tensor &init, const float &lr,
+           const int tmax) {
+  const int n_iter = 10000000;
 
-  //	torch::optim::SGD optimizer(
-  //		nca.parameters(),
-  //		torch::optim::SGDOptions(1e-5).momentum(0.5));
-  //
+  /*
+  torch::optim::SGD optimizer(
+    nca.parameters(),
+    torch::optim::SGDOptions(lr).momentum(0.5));
+    */
+
   for (int i = 0; i < n_iter; ++i) {
     Tensor state = init;
     state = state.to(options);
-    for (int t = 0; t < t_max; ++t) {
+    for (int t = 0; t < tmax; ++t) {
       state = nca.forward(state);
     }
     auto loss = mse_loss(target, state);
     auto g = torch::autograd::grad({loss}, {nca.fc1, nca.fc2});
+    if (i % 100 == 0) {
+      std::cout << "iter: " << i << " | loss: " << std::setprecision(4)
+                << loss.item<float>() << "\n";
+    }
     if (i % 1000 == 0) {
       std::cout << "========================================================="
                    "======================="
@@ -199,11 +200,25 @@ void train(NCA nca, const Tensor &target, const Tensor &init) {
                 << std::endl;
       std::cout << "fc2 gradient norm: " << torch::norm(g[1]).to(torch::kFloat)
                 << std::endl;
+
+      std::cout << "Writing images\n";
+
+      Tensor img = init;
+      img = img.to(options);
+      for (int t = 0; t < tmax; ++t) {
+        auto state_img =
+            img.index({0, Slice(), Slice(), Slice()}); // 3 = alpha channel
+        std::stringstream ss;
+        ss << "state_" << std::setfill('0') << std::setw(8) << i << "_"
+           << std::setw(3) << t << ".ppm";
+        std::ofstream ofile(ss.str());
+        if (ofile.is_open()) {
+          write_ppm(state_img, ofile);
+        }
+        img = nca.forward(img);
+      }
     }
 
-    // float lr = 1e-36;
-    // float lr = 0.0;
-    float lr = 1e-30;
     nca.fc1 = nca.fc1 - lr * g[0];
     nca.fc2 = nca.fc2 - lr * g[1];
   }
@@ -213,8 +228,10 @@ int main(int argc, char *argv[]) {
 
   std::cout << "getNumGPUs: " << torch::cuda::device_count() << std::endl;
 
+  const auto world_dim = WorldDim({4, 8, 8});
+
   // make target pattern
-  auto target = init_world(WorldDim{4, 6, 6});
+  auto target = init_world(world_dim);
   target.index_put_({0, 0, Slice(), 3}, 1.0); // 0 channel = red
   target.index_put_({0, 0, 3, Slice()}, 1.0);
   target.index_put_({0, 1, Slice(), 3}, 1.0); // 1 channel = green
@@ -225,8 +242,15 @@ int main(int argc, char *argv[]) {
   target.index_put_({0, 3, 3, Slice()}, 1.0);
   log("target", target);
 
+  auto target_img =
+      target.index({0, Slice(), Slice(), Slice()}); // 3 = alpha channel
+  std::ofstream ofile("target.ppm");
+  if (ofile.is_open()) {
+    write_ppm(target_img, ofile);
+  }
+
   // make initial condition
-  auto init = init_world(WorldDim{4, 6, 6});
+  auto init = init_world(world_dim);
   init.index_put_({0, 0, 3, 3}, 1.0); // red channel
   init.index_put_({0, 1, 3, 3}, 1.0); // red channel
   init.index_put_({0, 2, 3, 3}, 1.0); // red channel
@@ -237,17 +261,33 @@ int main(int argc, char *argv[]) {
       (4 + 2 * 4); // add 2 * 4 for horizontal and vertical sobel filters
   auto nca = NCA();
 
-  int hdim = 32;
+  const int hdim = 128;
+  const float lr = 1e-4;
+  const int tmax = 60;
 
   nca.fc1 = torch::zeros({hdim, fc1_input_dim}, options.requires_grad(true)) +
-            1e-3; // avoid exactly 0 due to relu
+            1000.0; // avoid exactly 0 due to relu
   nca.fc1 = torch::nn::init::kaiming_uniform_(nca.fc1);
   nca.fc2 = torch::zeros({4, hdim}, options.requires_grad(true));
 
   nca.fc1.to(torch::kCUDA);
   nca.fc2.to(torch::kCUDA);
 
-  train(nca, target, init);
+  std::cout << "hdim: " << hdim << std::endl;
+  std::cout << "lr: " << lr << std::endl;
+  std::cout << "tmax: " << tmax << std::endl;
+
+  train(nca, target, init, lr, tmax);
+
+  /*
+  auto test =
+      target.index({0, Slice(), Slice(), Slice()}); // 3 = alpha channel
+  std::cout << test;
+  std::ofstream ofile("test.ppm");
+  if (ofile.is_open()) {
+    write_ppm(test, ofile);
+  }
+  */
 
   std::cout << "Done" << std::endl;
 }
